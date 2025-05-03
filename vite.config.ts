1f89c8db777063f5d9a76b16f11bf640bf24819a
Sunny Kumar <155155751+sunnykumar917@users.noreply.github.com>
2025-05-03 15:43:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-{/*import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-})*/}
+})
 
 
-export default {
-  server: {
-    host: '0.0.0.0',  // Allow external access
-    port: process.env.PORT || 5173, // Use the environment variable for the port, default to 5173
-  },
-};
